55f010a34e58536c52033b8e51eba6b5dcd3b79a
arthurcezars <arthurcezars@gmail.com>
2021-09-26 11:17:57 -0300

    Adicionado pesquisa por estado

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template
+from doctest import debug
+
+from flask import Flask, render_template, request
 import  requests
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
+@app.route('/', methods=["GET", "POST"])
+def index():  # put application's code here
+    estado_escolhido = request.args.get("estados") # Pega o estado que o usuario pesquisar.
+
+    # Pesquisa na api que retorna o JSON com os dados.
     url = f"https://impostometro.com.br/Contador/Tributo?estado={'es'}&municipio={'vitoria'}&esfera=federal&tributo=cide&dataInicial=01/01/2021"
     response = requests.get(url)
     dados = response.json()
-    teste = ["https://impostometro.com.br/widget/contador/es?municipio=vitoria", "https://impostometro.com.br/widget/contador/es?municipio=serra"]
-    return render_template("index.html", impostos=teste)
+
+    print(estado_escolhido)
+    # Pesquisa na api que retorna o link para o iframe (somente visual, não retornar mais nenhum dado).
+    if (estado_escolhido == None):
+        tela_imposto = [f"https://impostometro.com.br/widget/contador/"]
+    else:
+        tela_imposto = [f"https://impostometro.com.br/widget/contador/{estado_escolhido}"]
+
+    return render_template("index.html", impostos=tela_imposto)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
