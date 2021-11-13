bbc840a4df8a3023728374fe523d067ec0b65021
arthurcezars <arthurcezars@gmail.com>
2021-11-13 15:35:31 -0300

    Adicionado controle de leds, aguardando teste

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from doctest import debug
 
 from flask import Flask, render_template, request
 import  requests
+#from led_control import temperatura
 
 app = Flask(__name__)
 
@@ -10,17 +11,22 @@ app = Flask(__name__)
 def index():  # put application's code here
     estado_escolhido = request.args.get("estados") # Pega o estado que o usuario pesquisar.
 
-    # Pesquisa na api que retorna o JSON com os dados.
-    url = f"https://impostometro.com.br/Contador/Tributo?estado={'es'}&municipio={'vitoria'}&esfera=federal&tributo=cide&dataInicial=01/01/2021"
-    response = requests.get(url)
-    dados = response.json()
-
     print(estado_escolhido)
     # Pesquisa na api que retorna o link para o iframe (somente visual, não retornar mais nenhum dado).
     if (estado_escolhido == None):
         tela_imposto = [f"https://impostometro.com.br/widget/contador/"]
+
+        #temperatura(5000000000)
     else:
         tela_imposto = [f"https://impostometro.com.br/widget/contador/{estado_escolhido.lower()}"]
+        # Pesquisa na api que retorna o JSON com os dados.
+        url = f"https://impostometro.com.br/Contador/Tributo?estado={estado_escolhido.lower()}&esfera=federal&tributo=cide&dataInicial=01/01/2021"
+        response = requests.get(url)
+        dados = response.json()
+
+        arrecadacao = int(dados['Valor'])
+        print(arrecadacao)
+        #temperatura(arrecadacao)
 
     return render_template("index.html", impostos=tela_imposto)
 
---
+++ led_control.py
@@ -0,0 +1,113 @@
+# Programa: Led enderecavel Neopixel com Raspberry Pi
+# Adaptacoes: Arduino e Cia
+# Baseado no programa exemplo da Adafruit
+
+import time
+import board
+import neopixel
+import RPi.GPIO as GPIO
+
+# Pino de conexao Raspberry
+# (podem ser utilizados os pinos 10, 12, 18 ou 21)
+pixel_pin = board.D18
+
+# Numero de leds que serao controlados
+num_pixels = 15
+
+# Ordem dos pixels de cor - RGB ou GRB
+# Em alguns módulos as cores verde e vermelha estao invertidas
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, auto_write=False,
+                           pixel_order=ORDER)
+
+
+def wheel(pos):
+    # Utiliza um valor entre 0 e 255 para definir a cor
+    # As cores sao uma transicao r - g - b
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER == neopixel.RGB or ORDER == neopixel.GRB else (r, g, b, 0)
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+def temperatura(arrecadacao):
+    try:
+        pixels.fill((0, 0, 0))
+        pixels.show()
+        while True:
+            print(arrecadacao)
+            if arrecadacao < 1000000:
+                for i in 5:
+                    pixels[i] = (0, 255, 0)
+            elif arrecadacao > 1000000 and arrecadacao < 1000000000:
+                for i in 10:
+                    if i <= 4:
+                        pixels[i] = (0, 255, 0)
+                    else:
+                        pixels[i] = (255, 255, 0)
+            else:
+                for i in 15:
+                    if i <= 4:
+                        pixels[i] = (0, 255, 0)
+                    elif i <= 9:
+                        pixels[i] = (255, 255, 0)
+                    else:
+                        pixels[i] = (255, 0, 0)
+
+    except KeyboardInterrupt:
+        # Apaga todos os leds
+        pixels.fill((0, 0, 0))
+        pixels.show()
+        GPIO.cleanup()
+
+try:
+    while True:
+        # Desabilite esta linha se for usar Neopixel RGBW/GRBW
+        pixels.fill((255, 0, 0))
+        # Habilite esta linha se for usar Neopixel RGBW/GRBW
+        # pixels.fill((255, 0, 0, 0))
+        pixels.show()
+        time.sleep(1)
+        # Desabilite esta linha se for usar Neopixel RGBW/GRBW
+        pixels.fill((0, 255, 0))
+        # Habilite esta linha se for usar Neopixel RGBW/GRBW
+        # pixels.fill((0, 255, 0, 0))
+        pixels.show()
+        time.sleep(1)
+        # Desabilite esta linha se for usar Neopixel RGBW/GRBW
+        pixels.fill((0, 0, 255))
+        # Habilite esta linha se for usar Neopixel RGBW/GRBW
+        # pixels.fill((0, 0, 255, 0))
+        pixels.show()
+        time.sleep(1)
+
+        # Efeito arco-iris com ciclo de 1ms por passo
+        rainbow_cycle(0.001)
+
+except KeyboardInterrupt:
+    # Apaga todos os leds
+    pixels.fill((0, 0, 0))
+    pixels.show()
+    GPIO.cleanup()
